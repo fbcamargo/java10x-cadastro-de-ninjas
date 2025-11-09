77985e15414cd5a8662b0df551abbce4008cecca
Felipe <camargo963@gmail.com>
2025-11-09 16:52:13 -0300

    refactor: Inclui a annotation ToString ao Controler Ninja e Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 @Data
 public class NinjaModel {
     @Id
